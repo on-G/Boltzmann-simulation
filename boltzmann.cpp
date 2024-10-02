c30cf159856581b75682c2540930a415c48d7867
on-G <riccardog003@gmail.com>
2024-10-02 18:09:28 +0200

    add boltzmann class and his constructor's definition and implementation.

--- boltzmann.cpp
+++ boltzmann.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "boltzmann.hpp"
+#include <iostream>
+#include <cmath>
+
+boltzmann::boltzmann() {
+    int N, T, U;
+    std::cout << "Inserisci il numero di sistemi da considerare: ";
+    std::cin >> N;
+    std::cout << "Inserisci il numero di cicli temporali: ";
+    std::cin >> T;
+    std::cout << "Inserisci l'energia media dell'ensemble: ";
+    std::cin >> U;
+
+    N_ = (int)std::round(N);
+    T_ = (int)std::round(T);
+    U_ = (int)std::round(U);
+
+    for(int i = 0; i < N_; ++i){
+        configuration_.push_back(U_);
+    }
+}
--- boltzmann.hpp
+++ boltzmann.hpp
@@ -5,4 +5,16 @@
 #ifndef BOLTZMANN_SIMULATION_BOLTZMANN_HPP
 #define BOLTZMANN_SIMULATION_BOLTZMANN_HPP
 
+#include <vector>
+
+class boltzmann{
+    std::vector<int> configuration_{};          //system's configuration
+    int U_{};           // energy
+    int N_{};           //number of systems
+    int T_{};           //time
+
+public:
+    boltzmann();
+};
+
 #endif //BOLTZMANN_SIMULATION_BOLTZMANN_HPP
